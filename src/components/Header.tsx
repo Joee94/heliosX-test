c5096a867115cceaa6d5ac0a9301b1e4199857f5
Joe Reddie <joseph.reddie@itv.com>
2025-04-03 21:46:36 +0100

    Add alt text to logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
   return (
     <header className="bg-medx-blue-dark w-full h-fit p-5">
-      <img src="/medexpress_logo.png" className="h-10" />
+      <img src="/medexpress_logo.png" className="h-10" alt="Medexpress logo" />
     </header>
   );
 };
